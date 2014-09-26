a448b21973396b199fc9e8bf3cccb6d3aa7b67d3
f3jbx6d <sachin.shetty@firstdata.com>
2014-09-26 10:22:07 -0400

    nodeJS added, php updated

---
+++ php/Payeezy/Payeezy.php
@@ -0,0 +1,332 @@
+<?php
+
+class Payeezy
+{
+  /**
+   * @var string The Payeezy API params to be used for requests.
+   */
+  public static $apiKey;
+  public static $apiSecret;
+  public static $merchantToken;
+  public static $baseURL;
+  public static $url;
+
+  /**
+   * Sets the API key to be used for requests.
+   *
+   * @param string $apiKey
+   */
+  public static function setApiKey($apiKey)
+  {
+    self::$apiKey = $apiKey;
+  }
+  /**
+   * Sets the API key to be used for requests.
+   *
+   * @param string $apiKey
+   */
+  public static function getApiKey()
+  {
+    return self::$apiKey;
+  }
+    /**
+   * Sets the API secret to be used for requests.
+   *
+   * @param string $apiSecret
+   */
+  public static function setApiSecret($apiSecret)
+  {
+    self::$apiSecret = $apiSecret;
+  }
+  /**
+   * Sets the API secret to be used for requests.
+   *
+   * @param string $apiSecret
+   */
+  public static function getApiSecret()
+  {
+    return self::$apiSecret;
+  }
+  /**
+   * Sets the API Base URL.
+   *
+   * @param string $url
+   */
+  public static function setUrl($baseURL)
+  {
+    self::$baseURL = $baseURL;
+  }
+  /**
+   * Gets the API Base URL.
+   *
+   * @param string $url
+   */
+  public static function getUrl()
+  {
+    return self::$baseURL;
+  }
+  /**
+   * Sets the API Merchant Token
+   *
+   * @param string $merchantToken
+   */
+  public static function setMerchantToken($merchantToken)
+  {
+    self::$merchantToken = $merchantToken;
+  }
+  /**
+   * Gets the API Merchant Token
+   *
+   * @param string $merchantToken
+   */
+  public static function getMerchantToken()
+  {
+    return self::$merchantToken;
+  }
+
+  /**
+   * Payeezy
+   * 
+   * Generate Payload
+   */
+
+  public function getPayload($args = array())
+  {
+    $args = array_merge(array(
+        "amount"=> "",
+        "card_number" => "", 
+        "card_type" => "", 
+        "card_holder_name" => "", 
+        "card_cvv" => "", 
+        "card_expiry" => "",
+        "merchant_ref" => "",
+        "currency_code" => "",
+        "transaction_tag" => "",
+        "split_shipment" => "",
+        "transaction_id" => "",
+
+    ), $args); 
+
+    $transaction_type = strtolower(func_get_arg(1));
+
+    $data = "";
+    if($transaction_type == ("authorize" || "purchase"))
+    {
+      $data = array(
+              'merchant_ref'=> $args['merchant_ref'],
+              'transaction_type'=> $transaction_type,
+              'method'=> 'credit_card',
+              'amount'=> $args['amount'],
+              'currency_code'=> strtoupper($args['currency_code']),
+              'credit_card'=> array(
+                      'type'=> $args['card_type'],
+                      'cardholder_name'=> $args['card_holder_name'],
+                      'card_number'=> $args['card_number'],
+                      'exp_date'=> $args['card_expiry'],
+                      'cvv'=> $args['card_cvv'],
+                    )
+      );
+
+      self::$url = self::$baseURL;
+    }else{
+
+      self::$url = self::$baseURL . '/' . $args['transaction_id'];
+
+      if($transaction_type == "split")
+      {
+
+        $data = array(
+          'merchant_ref'=> $args['merchant_ref'],
+          'transaction_type'=> $transaction_type,
+          'method'=> 'credit_card',
+          'amount'=> $args['amount'],
+          'currency_code'=> strtoupper($args['currency_code']),
+          'transaction_tag'=>$args['transaction_tag'],
+          'split_shipment'=>$args['split_shipment'],
+        );
+
+      }else{
+        $data = array(
+                'merchant_ref'=> $args['merchant_ref'],
+                'transaction_type'=> $transaction_type,
+                'method'=> 'credit_card',
+                'amount'=> $args['amount'],
+                'currency_code'=> strtoupper($args['currency_code']),
+                'transaction_tag'=>$args['transaction_tag'],
+        );
+
+      }
+    }
+    return json_encode($data, JSON_FORCE_OBJECT);
+  }
+
+  /**
+   * Payeezy
+   * 
+   * HMAC Authentication
+   */
+
+  public function hmacAuthorizationToken($payload)
+  {
+
+    $nonce = strval(hexdec(bin2hex(openssl_random_pseudo_bytes(4, $cstrong))));
+
+    $timestamp = strval(time());
+
+    $data = self::$apiKey + $nonce + $timestamp + self::$merchantToken + $payload;
+
+    $hashAlgorithm = "sha256";
+    // HMAC Hash in hex
+    $hmac = hash_hmac ( $hashAlgorithm , $data , self::$apiSecret, false );
+
+    $authorization = base64_encode($hmac);
+
+    return array(
+            'authorization' => $authorization,
+            'nonce' => $nonce,
+            'timestamp' => $timestamp,
+      ); 
+  }
+
+  /**
+   * jsonpp - Pretty print JSON data
+   *
+   * In versions of PHP < 5.4.x, the json_encode() function does not yet provide a
+   * pretty-print option. In lieu of forgoing the feature, an additional call can
+   * be made to this function, passing in JSON text, and (optionally) a string to
+   * be used for indentation.
+   *
+   * @param string $json  The JSON data, pre-encoded
+   * @param string $istr  The indentation string
+   *
+   * @return string
+   */
+  public function jsonpp($json, $istr='  ')
+  {
+      $result = '';
+      for($p=$q=$i=0; isset($json[$p]); $p++)
+      {
+          $json[$p] == '"' && ($p>0?$json[$p-1]:'') != '\\' && $q=!$q;
+          if(strchr('}]', $json[$p]) && !$q && $i--)
+          {
+              strchr('{[', $json[$p-1]) || $result .= "\n".str_repeat($istr, $i);
+          }
+          $result .= $json[$p];
+          if(strchr(',{[', $json[$p]) && !$q)
+          {
+              $i += strchr('{[', $json[$p])===FALSE?0:1;
+              strchr('}]', $json[$p+1]) || $result .= "\n".str_repeat($istr, $i);
+          }
+      }
+      return $result;
+  }
+
+  /**
+   * Payeezy
+   * 
+   * Post Transaction
+   */
+
+  public  function postTransaction($payload, $headers) 
+  {
+
+    $request = curl_init();
+    curl_setopt($request, CURLOPT_URL, self::$url );
+    curl_setopt($request, CURLOPT_POST, true);
+    curl_setopt($request, CURLOPT_POSTFIELDS, $payload);
+    curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($request, CURLOPT_HEADER, false);
+    curl_setopt($request, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json', 
+      'apikey:'.strval(self::$apiKey), 
+      'token:'.strval(self::$merchantToken), 
+      'Authorization:'.$headers['authorization'],
+      'nonce:'.$headers['nonce'],
+      'timestamp:'.$headers['timestamp'],
+    ));
+
+    $response = curl_exec($request);
+    curl_close($request);
+    $httpcode = curl_getinfo($response, CURLINFO_HTTP_CODE);
+    return $response;
+  }
+
+  /**
+   * Payeezy
+   * 
+   * Authorize Transaction
+   */
+
+  public function authorize($args = array())
+  {
+      $payload = $this->getPayload($args, "authorize");
+      $headerArray = $this->hmacAuthorizationToken($payload);
+      return $this->postTransaction($payload, $headerArray);
+  }
+
+  /**
+   * Payeezy
+   * 
+   * Purchase Transaction
+   */
+
+  public function purchase($args = array())
+  {
+      $payload = $this->getPayload($args, "purchase");
+      $headerArray = $this->hmacAuthorizationToken($payload);
+      return $this->postTransaction($payload, $headerArray);
+  }
+
+  /**
+   * Payeezy
+   * 
+   * Capture Transaction
+   */
+
+  public function capture($args = array())
+  {
+      $payload = $this->getPayload($args, "capture");
+      $headerArray = $this->hmacAuthorizationToken($payload);
+      return $this->postTransaction($payload, $headerArray);
+  }
+
+  /**
+   * Payeezy
+   * 
+   * Void Transaction
+   */
+
+  public function void($args = array())
+  {
+      $payload = $this->getPayload($args, "void");
+      $headerArray = $this->hmacAuthorizationToken($payload);
+      return $this->postTransaction($payload, $headerArray);
+  }
+
+  /**
+   * Payeezy
+   * 
+   * Refund Transaction
+   */
+
+  public function refund($args = array())
+  {
+      $payload = $this->getPayload($args, "refund");
+      $headerArray = $this->hmacAuthorizationToken($payload);
+      return $this->postTransaction($payload, $headerArray);
+  }
+
+  /**
+   * Payeezy
+   * 
+   * split Transaction
+   */
+
+  public function split_shipment($args = array())
+  {
+      $payload = $this->getPayload($args, "split");
+      $headerArray = $this->hmacAuthorizationToken($payload);
+      return $this->postTransaction($payload, $headerArray);
+  }
+
+}
--- php/test/example.php
+++ php/test/example.php
@@ -1,33 +1,265 @@
-<?php 
 
-define('__ROOT__', dirname(dirname(__FILE__))); 
-include(__ROOT__.'\htdocs\FirstApi\Firstapi\HTTP_Connection.php');
+<!DOCTYPE HTML> 
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+body {
+background: #336699 !important;
+font-family: "Lucida Grande", Tahoma, Arial, Verdana, sans-serif!important;
+font-size: small!important;
+margin: 8px 0 16px!important;
+text-align: left;
+}
 
-$apiKey = $_POST["apiKey"];
-$apiSecret = $_POST["apiSecret"];
-$gatewayID = $_POST["gatewayID"];
-$gatewayPass = $_POST["gatewayPassword"];
-$abp = new HTTP_Connection();
-$abp->_setAttr($apiKey,$apiSecret,$gatewayID,$gatewayPass);
-echo $abp->_getAttr();
-$url = 'https://demo26-test.apigee.net/firstapi-cat/transactions';
-$array = $abp->_makePayment($url, $abp->_generateCreditCardTestPayload());
+.title{
+padding: 15px;
+text-align: center;
+color: white;
+clear: left;font-size: 160%;
+font-weight: bold;
+background-color: #6699CC;
+}
+
+label.description {
+border: none;
+color: #222;
+display: block;
+font-size: 95%;
+font-weight: 700;
+line-height: 150%;
+padding: 0 0 1px;
+}
+
+</style>
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="all">
+<!-- <script type="text/javascript" src="js/bootstrap.js"></script> -->
+
+</head>
+<body> 
+
+<?php
+
+define('__ROOT__', dirname(dirname(__FILE__)));
+require_once(__ROOT__.'/Payeezy/Payeezy.php'); 
+
+// Initialise Payeezy
+$payeezy = new Payeezy();
+
+//Set Payeezy params
+$payeezy::$apiKey = "y6pWAJNyJyjGv66IsVuWnklkKUPFbb0a";
+$payeezy::$apiSecret = "86fbae7030253af3cd15faef2a1f4b67353e41fb6799f576b5093ae52901e6f7";
+$payeezy::$merchantToken = "fdoa-a480ce8951daa73262734cf102641994c1e55e7cdf4c02b6";
+$payeezy::$baseURL = "https://api-cert.payeezy.com/v1/transactions";
+
+$transaction_id = $transaction_tag = $transaction_type = $amount =  $split_shipment = "";
+$card_holder_name = $card_number = $card_type = $card_cvv = $card_expiry = "";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["transaction_type"])) {
+     $transaction_typeErr = "Transaction Type is required";
+   } else {
+     $transaction_type = test_input($_POST["transaction_type"]);
+     // check if name only contains letters and whitespace
+     if (!preg_match("/^[a-zA-Z ]*$/",$transaction_type)) {
+       $transaction_typeErr = "Only letters and white space allowed"; 
+     }
+   }
+
+  $transaction_id = test_input($_POST["transaction_id"]);
+  $transaction_tag = test_input($_POST["transaction_tag"]);
+  $card_holder_name = test_input($_POST["card_holder_name"]);
+  $card_number = test_input($_POST["card_number"]);
+  $card_type = test_input($_POST["card_type"]);
+  $card_cvv = test_input($_POST["card_cvv"]);
+  $card_expiry = test_input($_POST["card_expiry"]);
+  $amount = test_input($_POST["amount"]);
+  $split_shipment = test_input($_POST["split_shipment"]);
+
+
+  switch (strtolower($transaction_type)) {
+    case 'authorize':
+        $response_JSON = $payeezy->authorize(array(
+                                "amount"=> $amount,
+                                "card_number" => $card_number, 
+                                "card_type" => $card_type, 
+                                "card_holder_name" => $card_holder_name, 
+                                "card_cvv" => $card_cvv, 
+                                "card_expiry" => $card_expiry,
+                                "merchant_ref" => "Test Transaction",
+                                "currency_code" => "USD",
+                            ));
+      break;
+
+    case 'purchase':
+        $response_JSON = $payeezy->purchase(array(
+                                "amount"=> $amount,
+                                "card_number" => $card_number, 
+                                "card_type" => $card_type, 
+                                "card_holder_name" => $card_holder_name, 
+                                "card_cvv" => $card_cvv, 
+                                "card_expiry" => $card_expiry,
+                                "merchant_ref" => "Test Transaction",
+                                "currency_code" => "USD",
+                            ));
+      break;
+
+    case 'capture':
+        $response_JSON = $payeezy->capture(array(
+                                "amount"=> $amount,
+                                "transaction_tag" => $transaction_tag, 
+                                "transaction_id" => $transaction_id, 
+                                "merchant_ref" => "Test Transaction",
+                                "currency_code" => "USD",
+                            ));
+      break;
+
+    case 'void':
+        $response_JSON = $payeezy->void(array(
+                                "amount"=> $amount,
+                                "transaction_tag" => $transaction_tag, 
+                                "transaction_id" => $transaction_id, 
+                                "merchant_ref" => "Test Transaction",
+                                "currency_code" => "USD",
+                            ));
+      break;
+
+    case 'refund':
+        $response_JSON = $payeezy->refund(array(
+                                "amount"=> $amount,
+                                "transaction_tag" => $transaction_tag, 
+                                "transaction_id" => $transaction_id, 
+                                "merchant_ref" => "Test Transaction",
+                                "currency_code" => "USD",
+                            ));
+      break;
+
+    case 'split-shipment':
+        $response_JSON = $payeezy->split_shipment(array(
+                                "amount"=> $amount,
+                                "transaction_tag" => $transaction_tag, 
+                                "transaction_id" => $transaction_id, 
+                                "merchant_ref" => "Test Transaction",
+                                "currency_code" => "USD",
+                                "split_shipment" => $split_shipment,
+                            ));
+      break;
+    
+    default:
+      $transaction_typeErr = "Transaction Type Error. Please check and try again";
+      break;
+  }
+
+}
+
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return strval($data);
+}
 ?>
 
-<html> 
-<head> 
-<title>First Api</title> 
-</head> 
-<body> 
-<form method="post"  action="<?php echo htmlentities($_SERVER['PHP_SELF'])?>">
-<!-- //   -->
-Apigee ApiKey:<input type="text" name="apiKey"><br /> 
-Apigee ApiSecret:<input type="text" name="apiSecret"><br />
-SOA Gateway ID:<input type="text" name="gatewayID"><br /> 
-SOA Password:<input type="text" name="gatewayPassword"><br /> 
-<input type="submit" value="submit" name="submit1">
+<div class="row">
+  <div class="col-md-3"></div>
+  <div class="col-md-6" style="background:white;margin-top: 3%;">
+<div class="form_description">
+        <h2 class="title">Payeezy Example</h2><hr>
+        <p>Here is the example for Payeezy - PHP Client.<br>I Transaction ID &amp; Transaction Tag are required for Transaction Types: Capture, Void, Refund &amp; Split Shipment.<br>II Charge Card Details section is to be filled for Transaction Types: Authorize &amp; Purchase
+        </p>
+  <p><span class="error">* required field.</span></p>
+</div>
+
+<hr>
+<h5 style="margin-left: 2%;">Payeezy Transaction</h5><br>
+<form style="margin-left: 2%;" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+
+  <label class="decription">Transaction ID</label> <br>
+  <input type="text" name="transaction_id" value="<?php echo $transaction_id;?>" style="margin-top: -1%;">
+  <br><br>
+
+
+  <label class="decription">Transaction Tag</label> <br>
+  <input type="text" name="transaction_tag" value="<?php echo $transaction_tag;?>" style="margin-top: -1%;">
+  <br><br>
+
+
+  <label class="decription">Transaction Type</label>
+  <span class="error">* <?php echo $transaction_typeErr;?></span> <br>
+  <input type="radio" name="transaction_type" <?php if (isset($transaction_type) && $transaction_type=="authorize") echo "checked";?>  value="Authorize">  Authorize
+  <br><input type="radio" name="transaction_type" <?php if (isset($transaction_type) && $transaction_type=="purchase") echo "checked";?>  value="Purchase">  Purchase
+  <br><input type="radio" name="transaction_type" <?php if (isset($transaction_type) && $transaction_type=="capture") echo "checked";?>  value="Capture">  Capture
+  <br><input type="radio" name="transaction_type" <?php if (isset($transaction_type) && $transaction_type=="void") echo "checked";?>  value="Void">  Void
+  <br><input type="radio" name="transaction_type" <?php if (isset($transaction_type) && $transaction_type=="refund") echo "checked";?>  value="Refund">  Refund
+  <br><input type="radio" name="transaction_type" <?php if (isset($transaction_type) && $transaction_type=="split") echo "checked";?>  value="Split-Shipment">  Split-Shipment
+  <br><br>
+
+
+  <label class="decription">Split Shipment No.</label> <br>
+  <input type="text" name="split_shipment" value="<?php echo $split_shipment;?>" style="margin-top: -1%;">
+  <br><br>
+
+
+  <label class="decription">Amount</label> <br>
+  <input type="text" name="amount" value="<?php echo $amount;?>" style="margin-top: -1%;">
+  <br><br>
+
+
+  <hr>
+  <h5 style="margin-left: 2%;">Charge Card Details</h5><br>
+
+
+  <label class="decription">Card holder's Name</label> <br>
+  <input type="text" name="card_holder_name" value="<?php echo $card_holder_name;?>" style="margin-top: -1%;">
+  <br><br>
+
+
+  <label class="decription">Card Number</label>
+  <span class="error">   <?php echo $card_numberErr;?></span> <br>
+  <input type="text" name="card_number" value="<?php echo $card_number;?>" style="margin-top: -1%;">
+  <br><br>
+
+
+  <label class="decription">Card Type</label><br>
+  <input type="radio" name="card_type" <?php if (isset($card_type) && $card_type=="visa") echo "checked";?>  value="visa">  Visa
+  <br><input type="radio" name="card_type" <?php if (isset($card_type) && $card_type=="mastercard") echo "checked";?>  value="mastercard">  Master Card
+  <br><input type="radio" name="card_type" <?php if (isset($card_type) && $card_type=="american_express") echo "checked";?>  value="american_express">  American Express
+  <br><br>
+
+  <label class="decription">CVV</label> <br>
+  <input type="text" name="card_cvv" value="<?php echo $card_cvv;?>" style="margin-top: -1%;">
+  <br><br>
+
+
+  <label class="decription">Card Expiration (MMYY)</label> <br>
+  <input type="text" name="card_expiry" value="<?php echo $card_expiry;?>" style="margin-top: -1%;">
+  <br><br>
+
+   <br><br>
+   <input type="submit" name="submit" value="Submit"> 
 </form>
 
-<textarea><?php print_r($array) ?></textarea>
+<br><br>
+
+<div>
+  <h2 class="title">Response</h2><br>
+  <dl>
+      <dt>Type:</dt>
+      <dd><?php echo $transaction_type; ?><br></dd>
+      <dt>Result:</dt>
+      <dd><pre><?php echo $response_JSON; ?></pre></dd>
+  </dl>
+</div>
+
+</div>
+  <div class="col-md-3"></div>
+</div>
+
+
+
+
+
 </body>
 </html>
